a81ee90223ea7d90b80902dec928cd44b5cc939f
olajuwon74 <adebayosolomon74@gmail.com>
2022-06-27 22:55:02 +0100

    updated

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,4 +1,5 @@
 const {Flights} = require("../models/Flight");
+const {v4: uuid} = require("uuid");
 
 // get all flight
 exports.getFlights = async (req, res) => {
@@ -15,17 +16,53 @@ exports.getFlights = async (req, res) => {
 }
 
 // get single flight
+exports.getFlight = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const flight = Flights.find((flight) => user.id === id);
+            res.status(200).json({
+                message: "Flight details found",
+                flight,
+            });
+    } catch (err) {}
+}
 
 // update flight
 
+exports.updateFlight = async (req, res) =>{
+    try {
+        let id = req.params.id;
+        const flight = Flights.find((flight) => user.id === id);
+        const {title, time, price, date} = await req.body;
+        flight.title = title;
+        flight.time = time;
+        flight.price = price;
+        flight.date = date;
+        res.status(200).json({
+            message: "Flight updated",
+            flight,
+        })
+    } catch (err) {
+        
+    }
+}
 // add/book flight
 exports.createFlight = async (req, res) => {
     try {
-        const flight = await req.body;
-        Flights.push(flight)
+        // const flight = await req.body;
+        // user.id = uuid;
+        const {title, time, price, date} = await req.body;
+        const newFlight = {
+            id: uuid(),
+            title,
+            time,
+            price,
+            date,
+        };
+        Flights.push(newFlight)
         res.status(201).json({
             message: "Flight Created",
-            flight
+            newFlight,
         });
     } catch (err) {
         res.status(500).json({message:err})
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get("/", controller.getFlights)
-router.post("/", controller.createFlight)
+router
+.get("/", controller.getFlights)
+.post("/", controller.createFlight)
+.get("/:id", controller.getFlight)
+.put("/:id", controller.updateFlight);
+
 
 module.exports = router;
 
